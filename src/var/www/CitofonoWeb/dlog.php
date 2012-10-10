8dc231b7af64d18ac3c3aa10f29929c59d9ff2d4
Gabriele Martino <g.martino@gmx.com>
2012-10-10 13:32:38 +0200

    Log name is in $_GET

--- src/var/www/CitofonoWeb/dlog.php
+++ src/var/www/CitofonoWeb/dlog.php
@@ -1,15 +1,15 @@
 <?php
 include_once 'framework.php';
 
-if ($_POST['l']=='syslog')
+if ($_REQUEST['l']=='syslog')
 	$log=file_get_contents('/var/log/syslog');
-elseif ($_POST['l']=='badge_daemon')
+elseif ($_REQUEST['l']=='badge_daemon')
 	$log=file_get_contents('/var/log/badge_daemon.log');
 else
 	$log='';
 
 header('Content-type: text/plain');
-header('Content-Disposition: attachment; filename="'.$_GET['l'].'.log"');
+header('Content-Disposition: attachment; filename="'.$_REQUEST['l'].'.log"');
 header("Cache-Control: must-revalidate");
 header("Expires: ".gmdate("D, d M Y H:i:s",time()+(60 * 60 * 24 * 365 * 2)) . " GMT");
 echo $log;
