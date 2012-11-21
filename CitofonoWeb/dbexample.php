71d79b1d4969ca402ab3150f699b5e4703e9ae12
Gabriele Martino <g.martino@gmx.com>
2012-11-21 18:24:26 +0100

    Add "description" field

--- CitofonoWeb/dbexample.php
+++ CitofonoWeb/dbexample.php
@@ -7,12 +7,12 @@ $cms=new cms("Example data insertion",'');
 <h1>Example data insertion</h1>
 <p>
 <?php
-$insert="insert into `acl` (`badge_code`, `allowed`, `sched`) values ";
+$insert="insert into `acl` (`badge_code`, `description`, `allowed`, `sched`) values ";
 
 touch(config::localdb);
 try{
 	$local = new PDO('sqlite:'.config::localdb);
-	$query="CREATE TABLE IF NOT EXISTS acl (badge_code varchar(50) PRIMARY KEY, allowed tinyint, sched text)";
+	$query="CREATE TABLE IF NOT EXISTS acl (badge_code varchar(50) PRIMARY KEY, description varchar(50), allowed tinyint, sched text)";
 	$local->exec($query);
 }
 catch(PDOException $e){
@@ -20,10 +20,10 @@ catch(PDOException $e){
 }
 
 $insert.=<<<EOF
-('83', 1, '{"1":{"start":"090000","end":"180000"},"2":{"start":"090000","end":"180000"},"3":{"start":"090000","end":"180000"},"4":{"start":"090000","end":"180000"},"5":{"start":"090000","end":"180000"},"6":{"start":"000000","end":"000000"},"7":{"start":"000000","end":"000000"}}'),
-('107', 0, '{"1":{"start":"090000","end":"180000"},"2":{"start":"090000","end":"180000"},"3":{"start":"090000","end":"180000"},"4":{"start":"090000","end":"180000"},"5":{"start":"090000","end":"180000"},"6":{"start":"000000","end":"000000"},"7":{"start":"000000","end":"000000"}}'),
-('1000000022', 1, '{"1":{"start":"090000","end":"180000"},"2":{"start":"090000","end":"180000"},"3":{"start":"090000","end":"180000"},"4":{"start":"090000","end":"180000"},"5":{"start":"090000","end":"180000"},"6":{"start":"000000","end":"000000"},"7":{"start":"000000","end":"000000"}}'),
-('1000000012', 1, '{"1":{"start":"000001","end":"235959"},"2":{"start":"000001","end":"235959"},"3":{"start":"000001","end":"235959"},"4":{"start":"000001","end":"235959"},"5":{"start":"000001","end":"235959"},"6":{"start":"000001","end":"235959"},"7":{"start":"000001","end":"235959"}}')
+('83', '', 1, '{"1":{"start":"090000","end":"180000"},"2":{"start":"090000","end":"180000"},"3":{"start":"090000","end":"180000"},"4":{"start":"090000","end":"180000"},"5":{"start":"090000","end":"180000"},"6":{"start":"000000","end":"000000"},"7":{"start":"000000","end":"000000"}}'),
+('107', '', 0, '{"1":{"start":"090000","end":"180000"},"2":{"start":"090000","end":"180000"},"3":{"start":"090000","end":"180000"},"4":{"start":"090000","end":"180000"},"5":{"start":"090000","end":"180000"},"6":{"start":"000000","end":"000000"},"7":{"start":"000000","end":"000000"}}'),
+('1000000022', '', 1, '{"1":{"start":"090000","end":"180000"},"2":{"start":"090000","end":"180000"},"3":{"start":"090000","end":"180000"},"4":{"start":"090000","end":"180000"},"5":{"start":"090000","end":"180000"},"6":{"start":"000000","end":"000000"},"7":{"start":"000000","end":"000000"}}'),
+('1000000012', '', 1, '{"1":{"start":"000001","end":"235959"},"2":{"start":"000001","end":"235959"},"3":{"start":"000001","end":"235959"},"4":{"start":"000001","end":"235959"},"5":{"start":"000001","end":"235959"},"6":{"start":"000001","end":"235959"},"7":{"start":"000001","end":"235959"}}')
 EOF;
 
 try{
