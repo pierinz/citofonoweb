20c01ac42a9f6230dc06a145b35c102438897417
Gabriele Martino <g.martino@gmx.com>
2012-11-22 14:17:10 +0100

    Added some comments

--- script/db_update.sh
+++ script/db_update.sh
@@ -2,7 +2,8 @@
 #Update DB to the latest version
 dbfile="$1"
 
-#If database doesn't exist, the user needs to configure the software and generate one later 
+#If database doesn't exist, do nothing 
+#the user needs to configure the software and generate one later
 if [ -f "$1" ]; then
 	#Try to add any missing field
 	echo 'alter table acl add column description varchar(50);' | sqlite3 $1 2>/dev/null
