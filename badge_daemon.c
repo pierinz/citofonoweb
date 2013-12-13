dddebdfd3e02a18d690718b01af557d8b663259b
Gabriele Martino <g.martino@gmx.com>
2013-12-13 18:59:51 +0100

    Remove unused function

--- badge_daemon.c
+++ badge_daemon.c
@@ -71,38 +71,6 @@ char** argv_from_string(char *args) {
     return argv;
 }
 
-size_t trimwhitespace(char *out, size_t len, const char *str){
-    const char *end;
-    size_t out_size;
-
-    
-    if(len == 0)
-      return 0;
-
-    /* Trim leading space */
-    while(isspace(*str)) str++;
-
-    if(*str == 0)  /* All spaces? */
-    {
-      *out = 0;
-      return 1;
-    }
-
-    /* Trim trailing space */
-    end = str + strlen(str) - 1;
-    while(end > str && isspace(*end)) end--;
-    end++;
-
-    /* Set output size to minimum of trimmed string length and buffer size minus 1 */
-    out_size = (end - str) < len-1 ? (end - str) : len-1;
-
-    /* Copy trimmed string and add null terminator */
-    memcpy(out, str, out_size);
-    out[out_size] = 0;
-
-    return out_size;
-}
-
 void logmessage(char *message){
     char buffer[22];
     time_t rawtime;
