b37a36f5405aed0c9f6eab35fc0269aa78502e36
Gabriele Martino <g.martino@gmx.com>
2016-06-07 15:27:48 +0200

    Translate comments

--- badge_uploader.c
+++ badge_uploader.c
@@ -201,7 +201,7 @@ int main(int argc, char *argv[]) {
 		msync(queue, msize, MS_SYNC);
 	}
 
-	/* Cattura segnali di uscita */
+	/* Catch exit signals */
 	sig_h.sa_handler = signal_handler;
 	sig_h.sa_flags = 0;
 	/* Signals blocked during the execution of the handler. */
